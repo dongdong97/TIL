e2bac6efdcdc22dddbd40ba4ca635db01293220f
동균 <hdg9455@naver.com>
2018-07-05 17:48:56 +0900

    180705 TIL

---
+++ Algorithm_BaekJoon/15813.cpp
@@ -0,0 +1,27 @@
+//
+//  15813.cpp
+//  Algorithm
+//
+//  Created by 동균 on 2018. 7. 5..
+//  Copyright © 2018년 동균. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int length;
+    string name;
+    int sum = 0;
+    
+    cin >> length;
+    cin >> name;
+    
+    for(int i=0;i<length;i++) {
+        sum += (int)name[i] - 64;
+
+    }
+    cout << sum;
+    return 0;
+}
